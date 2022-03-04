1c033fe5103ae634ff60bc2065a9d836ffe4df9b
takumi ikeda <bshuge02@gmail.com>
2022-03-05 04:54:04 +0900

    fix: ./deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,10 @@ dotfiles=(.zshrc .vimrc)
 
 # 配置したい設定ファイルのシンボリックリンクをホームディレクトリ配下に作成する
 
-for file in "${dotfiles[@]}"; do
-	ln -svf $file ~/
+for file in "${dotfiles[@]}";
+do
+	echo $file
+	ln -svf ~/dotfiles/$file ~/
 done
 
 
