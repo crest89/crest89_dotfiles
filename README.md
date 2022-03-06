e020144dbafe4098f0066efebcddb1358ce4af3a
takumi ikeda <bshuge02@gmail.com>
2022-03-06 22:56:42 +0900

    add zshfile

---
+++ README.md
@@ -0,0 +1,1 @@
+# crest89_dotfiles
---
+++ zsh/plugin.zsh
@@ -0,0 +1,12 @@
+# IMPORTS
+source ~/.zplug/init.zsh
+
+# PLUGIN
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "b4b4r07/zsh-vimode-visual", defer:3
+zplug "zsh-users/zsh-completions"
+zplug "b4b4r07/enhancd", use:init.sh
+zplug "zsh-users/zsh-autosuggestions"
+zplug romkatv/powerlevel10k, as:theme, depth:1
+zplug load
+setopt nonomatch
